0bb3416a9da1a665bf05e7ceddf2521099b05e92
XiCheng148 <1284670996@qq.com>
2024-10-16 15:55:07 +0800

    fix(components/Button): 修改按钮组件在新标签页和当前标签页打开链接的行为

--- components/Button.vue
+++ components/Button.vue
@@ -27,20 +27,20 @@ const buttonConfig = {
     desc: '新标签中打开',
     show: false,
     click: () => {
-      window.open(window.location.href);
+      window.open(removeSmartPopupParam(window.location.href));
       window.close();
     },
   },
-  openInCurrentTab: {
-    icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" ><path fill="currentColor" d="m160 96.064 192 .192a32 32 0 0 1 0 64l-192-.192V352a32 32 0 0 1-64 0V96h64zm0 831.872V928H96V672a32 32 0 1 1 64 0v191.936l192-.192a32 32 0 1 1 0 64zM864 96.064V96h64v256a32 32 0 1 1-64 0V160.064l-192 .192a32 32 0 1 1 0-64l192-.192zm0 831.872-192-.192a32 32 0 0 1 0-64l192 .192V672a32 32 0 1 1 64 0v256h-64z"></path></svg>',
-    text: 'openInCurrentTab',
-    desc: '当前标签中打开',
-    show: false,
-    click: () => {
-      window.open(window.location.href, '_self');
-      window.close();
-    },
-  },
+  // openInCurrentTab: {
+  //   icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" ><path fill="currentColor" d="m160 96.064 192 .192a32 32 0 0 1 0 64l-192-.192V352a32 32 0 0 1-64 0V96h64zm0 831.872V928H96V672a32 32 0 1 1 64 0v191.936l192-.192a32 32 0 1 1 0 64zM864 96.064V96h64v256a32 32 0 1 1-64 0V160.064l-192 .192a32 32 0 1 1 0-64l192-.192zm0 831.872-192-.192a32 32 0 0 1 0-64l192 .192V672a32 32 0 1 1 64 0v256h-64z"></path></svg>',
+  //   text: 'openInCurrentTab',
+  //   desc: '当前标签中打开',
+  //   show: false,
+  //   click: () => {
+  //     window.open(window.location.href, '_self');
+  //     window.close();
+  //   },
+  // },
 };
 
 const activeDesc = ref('');
